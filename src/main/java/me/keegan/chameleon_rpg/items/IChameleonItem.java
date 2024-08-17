1b0069a9a47fed8e71fb8d1bd5030e8fa5974436
klb6542 <keeganthebates2023@gmail.com>
2024-08-17 15:31:38 -0400

    Refactor

--- src/main/java/me/keegan/chameleon_rpg/items/IChameleonItem.java
+++ src/main/java/me/keegan/chameleon_rpg/items/IChameleonItem.java
@@ -1,6 +1,6 @@
 package me.keegan.chameleon_rpg.items;
 
-import me.keegan.chameleon_rpg.utils.namespacedkeys.IChameleonNamespaced;
+import me.keegan.chameleon_rpg.utils.game.namespacedkeys.IChameleonNamespaced;
 
 public interface IChameleonItem extends IChameleonNamespaced<String> {
     ChameleonItemStackWrapper createItemStackWrapper();
