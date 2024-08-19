50aff3bb3dc8bdf8850ff0018c666c726ecf4832
klb6542 <keeganthebates2023@gmail.com>
2024-08-18 21:46:43 -0400

    Added new method

--- src/main/java/me/keegan/chameleon_rpg/utils/game/ChameleonChat.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/game/ChameleonChat.java
@@ -1,6 +1,8 @@
 package me.keegan.chameleon_rpg.utils.game;
 
 import me.keegan.chameleon_rpg.ChameleonRPG;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -13,6 +15,10 @@ public final class ChameleonChat {
 
     public static void sendMessage(Player player, String message) {
         player.sendMessage(addMessagePrefix(message));
+    }
+
+    public static void sendActionBarMessage(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
     }
 
     public static void sendBroadcast(String message) {
