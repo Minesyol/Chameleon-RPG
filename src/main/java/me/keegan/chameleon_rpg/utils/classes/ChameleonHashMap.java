104670fb50bfc89c42d6bff8239629e0b8038a97
klb6542 <keeganthebates2023@gmail.com>
2024-08-18 21:47:08 -0400

    Added

---
+++ src/main/java/me/keegan/chameleon_rpg/utils/classes/ChameleonHashMap.java
@@ -0,0 +1,12 @@
+package me.keegan.chameleon_rpg.utils.classes;
+
+import java.util.HashMap;
+
+public final class ChameleonHashMap<K, V> extends HashMap<K, V> {
+    public ChameleonHashMap() {}
+
+    public void putIfExists(K key, V value) {
+        if (super.containsKey(key))
+            super.put(key, value);
+    }
+}
