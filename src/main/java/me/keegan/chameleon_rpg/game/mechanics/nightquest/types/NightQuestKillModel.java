bbe0b92eddc4cfb6f1106ebf71a2ed73cd83a196
qsmh6542 <keeganlbates2022@gmail.com>
2024-08-20 08:50:12 -0400

    Implemented stuff

--- src/main/java/me/keegan/chameleon_rpg/game/mechanics/nightquest/types/NightQuestKillModel.java
+++ src/main/java/me/keegan/chameleon_rpg/game/mechanics/nightquest/types/NightQuestKillModel.java
@@ -1,6 +1,10 @@
 package me.keegan.chameleon_rpg.game.mechanics.nightquest.types;
 
 import me.keegan.chameleon_rpg.game.mechanics.nightquest.NightQuestModel;
+import org.bukkit.entity.Player;
 
 public class NightQuestKillModel extends NightQuestModel {
+    public NightQuestKillModel(Player player) {
+        super(player);
+    }
 }
--- src/main/java/me/keegan/chameleon_rpg/game/mechanics/nightquest/types/NightQuestMineModel.java
+++ src/main/java/me/keegan/chameleon_rpg/game/mechanics/nightquest/types/NightQuestMineModel.java
@@ -1,6 +1,10 @@
 package me.keegan.chameleon_rpg.game.mechanics.nightquest.types;
 
 import me.keegan.chameleon_rpg.game.mechanics.nightquest.NightQuestModel;
+import org.bukkit.entity.Player;
 
 public class NightQuestMineModel extends NightQuestModel {
+    public NightQuestMineModel(Player player) {
+        super(player);
+    }
 }
