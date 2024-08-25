d35152300d78fc617f0e3a3a1fdde899026ee213
klb6542 <keeganthebates2023@gmail.com>
2024-08-24 22:42:41 -0400

    Simplified and finished

--- src/main/java/me/keegan/chameleon_rpg/utils/events/ChameleonEvent.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/events/ChameleonEvent.java
@@ -6,16 +6,6 @@ import org.bukkit.event.Listener;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class ChameleonEvent extends Event implements Listener {
-    private final ChameleonEvent event;
-
-    protected ChameleonEvent() {
-        event = this;
-    }
-
-    public ChameleonEvent getEvent() {
-        return event;
-    }
-
     @NonNull
     public HandlerList getHandlers() {
         return new HandlerList();
