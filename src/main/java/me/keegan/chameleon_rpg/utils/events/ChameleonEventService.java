65f688c3ef04cbdf3e50badb5b7a9f96e18a3caa
klb6542 <keeganthebates2023@gmail.com>
2024-08-24 22:42:57 -0400

    Testing

--- src/main/java/me/keegan/chameleon_rpg/utils/events/ChameleonEventService.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/events/ChameleonEventService.java
@@ -1,8 +1,38 @@
 package me.keegan.chameleon_rpg.utils.events;
 
-public final class ChameleonEventService {
-    public static <T> void callEvent(ChameleonEvent<T> chameleonEvent) {
+import me.keegan.chameleon_rpg.ChameleonRPG;
+import me.keegan.chameleon_rpg.utils.files.registeries.Registries;
+import me.keegan.chameleon_rpg.utils.interfaces.IChameleonPluginState;
+import org.bukkit.event.EventHandler;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class ChameleonEventService implements IChameleonPluginState {
+    private static final List<Method> eventMethods = new ArrayList<>();
+
+    public static void callEvent(ChameleonEvent chameleonEvent) {
+        if (chameleonEvent instanceof EntityDeathByPlayerEvent e) {
+            ChameleonRPG.info(e.getVictim().getName());
+            ChameleonRPG.info(e.getKiller().getName());
+        }
+    }
+
+    @Override
+    public void onPluginEnable() {
+        Registries.registerReflections(Registries.Scanner.SUBTYPES, IChameleonListener.class, registry -> {
+            for (Method method : registry.getClass().getMethods()) {
+                if (Arrays.stream(method.getAnnotations()).noneMatch(annotation -> annotation instanceof EventHandler)
+                        || method.getParameterCount() != 1
+                        || !this.getClass().isAssignableFrom(method.getParameterTypes()[0])) {
+                    continue;
+                }
+
+                eventMethods.add(method);
+            }
+        });
     }
 
 }
