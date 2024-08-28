de809b6dcb5f10f746964766f9d8e1454b2f5614
qsmh6542 <keeganlbates2022@gmail.com>
2024-08-28 14:28:13 -0400

    Commit

---
+++ src/main/java/me/keegan/chameleon_rpg/utils/game/items/MysticUtils.java
@@ -0,0 +1,27 @@
+package me.keegan.chameleon_rpg.utils.game.items;
+
+import me.keegan.chameleon_rpg.game.items.mystics.CustomMystic;
+import me.keegan.chameleon_rpg.game.items.mystics.Mystic;
+import me.keegan.chameleon_rpg.utils.files.SerializationUtils;
+import me.keegan.chameleon_rpg.utils.game.namespacedkeys.ChameleonNamespacedKeys;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.units.qual.C;
+
+public final class MysticUtils {
+    public static void saveMysticData(@NonNull ItemStack mystic, @NonNull CustomMystic customMystic) {
+        String encodedCustomMystic = SerializationUtils.encodeToBase64(customMystic);
+
+        PersistentDataContainer persistentDataContainer = mystic.getItemMeta().getPersistentDataContainer();
+        persistentDataContainer.set(ChameleonNamespacedKeys.MYSTIC_ITEMSTACK_KEY, PersistentDataType.STRING, encodedCustomMystic);
+    }
+
+    @Nullable
+    public static CustomMystic getMysticData(@NonNull ItemStack mystic) {
+
+    }
+
+}
--- src/main/java/me/keegan/chameleon_rpg/utils/game/namespacedkeys/ChameleonNamespacedKeys.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/game/namespacedkeys/ChameleonNamespacedKeys.java
@@ -9,6 +9,7 @@ import javax.annotation.Nullable;
 
 public final class ChameleonNamespacedKeys {
     public static final NamespacedKey CHAMELEON_ITEMSTACK_KEY = new NamespacedKey(ChameleonRPG.getPlugin(), "item_identifier");
+    public static final NamespacedKey MYSTIC_ITEMSTACK_KEY = new NamespacedKey(ChameleonRPG.getPlugin(), "mystic");
 
     @Nullable
     public static <P, C> Object getNamespacedValue(NamespacedKey namespacedKey, PersistentDataType<P, C> persistentDataType, PersistentDataContainer persistentDataContainer) {
