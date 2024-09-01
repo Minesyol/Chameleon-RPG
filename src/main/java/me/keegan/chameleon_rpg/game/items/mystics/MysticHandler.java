0b95f741b57cc300ae7c4a71ec81bc844cae4f21
klb6542 <keeganthebates2023@gmail.com>
2024-09-01 19:37:07 -0400

    Worked on

--- src/main/java/me/keegan/chameleon_rpg/game/items/mystics/MysticHandler.java
+++ src/main/java/me/keegan/chameleon_rpg/game/items/mystics/MysticHandler.java
@@ -1,12 +1,18 @@
 package me.keegan.chameleon_rpg.game.items.mystics;
 
+import me.keegan.chameleon_rpg.game.items.mystics.types.Mystic;
 import me.keegan.chameleon_rpg.utils.events.IChameleonListener;
-import me.keegan.chameleon_rpg.utils.events.model.types.EntityDeathByPlayerCEvent;
+import me.keegan.chameleon_rpg.utils.events.model.types.EntityDeathByCPlayerCEvent;
 import me.keegan.chameleon_rpg.utils.game.items.MysticUtils;
-import org.bukkit.Material;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class MysticHandler implements IChameleonListener {
     @EventHandler
@@ -16,7 +22,31 @@ public class MysticHandler implements IChameleonListener {
     }
 
     @EventHandler
-    public void onEntityDeathByPlayer(EntityDeathByPlayerCEvent e) {
-
+    public void onPrepareItemEnchant(PrepareItemEnchantEvent e) {
+        if (!MysticUtils.isMystic(e.getItem())) { return; }
+        e.setCancelled(true);
     }
+
+    public static class MysticDrops implements IChameleonListener{
+        /**
+         *
+         * @param player Player to send heads-up to
+         * @param location Location where mystic will be dropped
+         */
+        // TODO: implement sound and finish creating the mystics
+        private void dropMystic(Player player, Location location) {
+            location.add(0, 1, 0);
+
+            World world = player.getWorld();
+
+            world.spawnParticle(Particle.FALLING_WATER, location, 225, 0.3, 0.26, 0.3); // i = amount
+            world.dropItem(location, MysticFactory.createMystic(Mystic.DEFAULT, (ChatColor) Mystic.DEFAULT.getPantColors().keySet().toArray()[(new Random().nextInt(Mystic.DEFAULT.getPantColors().size() - 1))]));
+        }
+
+        @EventHandler
+        public void onEntityDeathByPlayer(EntityDeathByCPlayerCEvent e) {
+            dropMystic(e.getKiller(), e.getVictim().getLocation());
+        }
+    }
+
 }
