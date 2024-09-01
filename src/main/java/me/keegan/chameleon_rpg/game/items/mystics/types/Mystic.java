37ab5eafce9a440cd1379f865011ac59ad301c49
klb6542 <keeganthebates2023@gmail.com>
2024-09-01 19:36:42 -0400

    Implemented DEFAULT methods

--- src/main/java/me/keegan/chameleon_rpg/game/items/mystics/types/Mystic.java
+++ src/main/java/me/keegan/chameleon_rpg/game/items/mystics/types/Mystic.java
@@ -1,89 +1,119 @@
 package me.keegan.chameleon_rpg.game.items.mystics.types;
 
+import me.keegan.chameleon_rpg.utils.classes.builders.LoreBuilder;
+import me.keegan.chameleon_rpg.utils.classes.string.Inflector;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 public enum Mystic implements IMystic {
-    FRESH {
+    DEFAULT {
         @Override
         public ItemStack setFreshData(ItemStack itemStack) {
-            return null;
+            return itemStack;
         }
 
         @Override
-        public List<String> getFreshLore(@Nullable ChatColor chatColor) {
-            return List.of();
+        public List<String> getFreshLore(ChatColor chatColor) {
+            return new LoreBuilder()
+                    .write("Kept on death")
+                    .newLine()
+                    .newLine().setColor(chatColor).write("Used in the mystic well")
+                    .newLine().write("Also, a fashion statement")
+                    .build();
         }
 
         @Override
-        public List<String> getEnchantedLore(@Nullable ChatColor chatColor) {
-            return List.of();
+        public List<String> getEnchantedLore(ChatColor chatColor) {
+            return new LoreBuilder()
+                    .setColor(chatColor).write("As strong as iron")
+                    .build();
         }
 
         @Override
         public Material getMaterial() {
-            return null;
+            return Material.LEATHER_LEGGINGS;
         }
 
         @Override
         public String getFreshPrefix() {
-            return "";
+            return "Fresh";
         }
 
         @Override
         public String getName(ChatColor chatColor) {
-            return "";
+            return (this.getPantColors().containsKey(chatColor))
+                    ? Inflector.getInstance().titleCase(chatColor.name().replace("GOLD", "ORANGE")) + " Pants"
+                    : "";
         }
 
         @Override
         public String getEnumName() {
-            return "";
+            return this.name();
         }
 
         @Override
         public MysticRequiredPantColor getRequiredPantColor() {
-            return null;
+            return MysticRequiredPantColor.SAME_COLOR;
         }
 
         @Override
         public LinkedHashMap<ChatColor, Color> getPantColors() {
-            return null;
+            return new LinkedHashMap<>() {{
+                put(ChatColor.RED, Color.fromRGB(255, 85, 85));
+                put(ChatColor.GOLD, Color.fromRGB(255, 170, 0));
+                put(ChatColor.YELLOW, Color.fromRGB(255, 255, 85));
+                put(ChatColor.GREEN, Color.fromRGB(85, 255, 85));
+                put(ChatColor.BLUE, Color.fromRGB(85, 85, 255));
+            }};
         }
 
         @Override
         public List<ItemStack> getMysticWellPaneColors() {
-            return List.of();
+            return Arrays.asList(
+                    new ItemStack(Material.PINK_STAINED_GLASS),
+                    new ItemStack(Material.LIME_STAINED_GLASS),
+                    new ItemStack(Material.YELLOW_STAINED_GLASS),
+                    new ItemStack(Material.RED_STAINED_GLASS));
         }
 
         @Override
-        public List<ChatColor> getTierColors(@Nullable ChatColor chatColor) {
-            return List.of();
+        public List<ChatColor> getTierColors(ChatColor chatColor) {
+            return Arrays.asList(
+                    chatColor,
+                    chatColor,
+                    chatColor,
+                    chatColor
+            );
         }
 
         @Override
         public List<Integer> getMysticWellCost() {
-            return List.of();
+            return Arrays.asList(
+                    1000,
+                    4000,
+                    8000
+            );
         }
 
         @Override
         public int getMaxTier() {
-            return 0;
+            return 3;
         }
 
         @Override
         public boolean requireSacrificeOnMaxTier() {
-            return false;
+            return true;
         }
 
         @Override
         public boolean isStrongAsIron() {
-            return false;
+            return true;
         }
-    };
+    },
 }
