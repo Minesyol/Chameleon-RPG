44d29a90ecd07cbc49425d0bbb394cb4924a1341
klb6542 <keeganthebates2023@gmail.com>
2024-09-09 22:22:31 -0400

    Fixed method logic

--- src/main/java/me/keegan/chameleon_rpg/utils/events/model/types/LivingEntityDamageByLivingEntityCEvent.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/events/model/types/LivingEntityDamageByLivingEntityCEvent.java
@@ -6,13 +6,16 @@ import me.keegan.chameleon_rpg.utils.events.model.ChameleonEvent;
 import me.keegan.chameleon_rpg.utils.game.entity.ChameleonEquipmentHands;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 @Getter
 public class LivingEntityDamageByLivingEntityCEvent extends ChameleonEvent<EntityDamageByEntityEvent> {
     private final ChameleonEquipmentHands attackerHands;
     private final LivingEntity attacker;
     private final LivingEntity victim;
+    private final double finalDamage;
 
     public LivingEntityDamageByLivingEntityCEvent(EntityDamageByEntityEvent e) {
         super(e);
@@ -20,6 +23,11 @@ public class LivingEntityDamageByLivingEntityCEvent extends ChameleonEvent<Entit
         this.attacker = (LivingEntity) e.getDamager();
         this.victim = (LivingEntity) e.getEntity();
         this.attackerHands = new ChameleonEquipmentHands(attacker.getEquipment().getItemInMainHand(), attacker.getEquipment().getItemInOffHand());
+        this.finalDamage = e.getFinalDamage();
+    }
+
+    public double getDamage(EntityDamageEvent.DamageModifier damageModifier) {
+        return super.getEvent().getDamage(damageModifier);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -29,8 +37,8 @@ public class LivingEntityDamageByLivingEntityCEvent extends ChameleonEvent<Entit
          * from this, which can call the event twice.
          */
         if (!(e.getDamager() instanceof LivingEntity
-                || e.getEntity() instanceof LivingEntity
-                || this.getClass().equals(LivingEntityDamageByLivingEntityCEvent.class))) { return; }
+                && e.getEntity() instanceof LivingEntity
+                && super.isCalledInSuperClass(LivingEntityDamageByLivingEntityCEvent.class, this.getClass()))) { return; }
         ChameleonEventService.callEvent(new LivingEntityDamageByLivingEntityCEvent(e));
     }
 }
