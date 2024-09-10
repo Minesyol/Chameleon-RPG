04ebcece3fb616c7b5428e33e1c098c308f8d04c
klb6542 <keeganthebates2023@gmail.com>
2024-09-09 22:19:12 -0400

    Added method

--- src/main/java/me/keegan/chameleon_rpg/utils/events/model/ChameleonEvent.java
+++ src/main/java/me/keegan/chameleon_rpg/utils/events/model/ChameleonEvent.java
@@ -1,11 +1,11 @@
 package me.keegan.chameleon_rpg.utils.events.model;
 
+import me.keegan.chameleon_rpg.utils.events.IChameleonListener;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public class ChameleonEvent<T extends Event> extends Event implements Listener {
+public class ChameleonEvent<T extends Event> extends Event implements IChameleonListener {
     private final T event;
 
     public ChameleonEvent(T event) {
@@ -14,6 +14,19 @@ public class ChameleonEvent<T extends Event> extends Event implements Listener {
 
     public T getEvent() {
         return event;
+    }
+
+    /**
+     *
+     *
+     * @param declaredClass The class where the method was originally declared
+     * @param currentClass The current class (this.getClass())
+     * @return True if the method was called in the class that declared it,
+     *      in others words it returns false if the method was called and if it's
+     *      inherited from a super class.
+     */
+    public final boolean isCalledInSuperClass(Class<?> declaredClass, Class<?> currentClass) {
+        return currentClass.getName().equals(declaredClass.getName());
     }
 
     @NonNull
